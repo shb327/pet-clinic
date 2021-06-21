a8bd0f2509c3b4912e7fb12a886bc3a6da349ae7
shb327 <shkamarida.b@gmail.com>
2021-06-21 17:25:19 +0300

    🎉

--- src/main/java/com/aboba/petclinic/model/User.java
+++ src/main/java/com/aboba/petclinic/model/User.java
@@ -1,11 +1,18 @@
 package com.aboba.petclinic.model;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.NotFound;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 
 /**
  * Simple JavaBean domain object representing a normal application user.
@@ -22,14 +29,37 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String username;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private String authorities;
+
+    @NotNull
+    @Size(min = 2, max = 20)
     private String firstName;
+
+    @NotNull
+    @Size(min = 5, max = 30)
     private String email;
+
+    @NotNull
+    @Positive
     private Integer age;
+
+    @NotNull
+    @Size(min = 2, max = 20)
     private String lastName;
+
+    @NotNull
+    @Size(min = 2, max = 30)
     private String address;
+
+    @NotNull
+    @Size(min = 2, max = 30)
     private String phoneNumber;
 }
